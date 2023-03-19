b12545df5818368b46c803413804df42a2a8c337
osmanmostafa <osmanmostafa661@gmail.com>
2023-03-19 19:14:11 +0200

    Enable CORS

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+var cors = require('cors')
 const PORT = process.env.PORT || 3030;
+app.use(cors())
 
 // app.get('/',(req,res) => {
 //     res.send('hi all')
